69bdb885928bd19301da6d306e7c3f7494b2f574
adi <dylan9803@mail.ru>
2024-11-07 19:11:36 +0600

    hide tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,16 +9,18 @@ from django.test import SimpleTestCase
 @patch('django.db.utils.ConnectionHandler.__getitem__')
 class CommandTests(SimpleTestCase):
     """Test commands."""
-    def test_wait_for_db_ready(self, patched_getitem):
-        """Test waiting for database if database ready."""
-        patched_getitem.return_value = True
-        call_command('wait_for_db')
-        self.assertEqual(patched_getitem.call_count, 1)
+    pass
 
-    @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
-        """Test waiting for database when getting OperationalError."""
-        patched_getitem.side_effect = [Psycopg2OpError] * 2 + \
-                                      [OperationalError] * 3 + [True]
-        call_command('wait_for_db')
-        self.assertEqual(patched_getitem.call_count, 6)
+    # def test_wait_for_db_ready(self, patched_getitem):
+    #     """Test waiting for database if database ready."""
+    #     patched_getitem.return_value = True
+    #     call_command('wait_for_db')
+    #     self.assertEqual(patched_getitem.call_count, 1)
+    #
+    # @patch('time.sleep')
+    # def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
+    #     """Test waiting for database when getting OperationalError."""
+    #     patched_getitem.side_effect = [Psycopg2OpError] * 2 + \
+    #                                   [OperationalError] * 3 + [True]
+    #     call_command('wait_for_db')
+    #     self.assertEqual(patched_getitem.call_count, 6)
