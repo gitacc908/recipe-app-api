9e1798b393a647dbdd8bf304ecc40912e609677f
adi <dylan9803@mail.ru>
2024-11-07 19:09:49 +0600

    configured checks for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-r5xy3vnwyj%i*4t)fqcf-&7boe1f9p9*m!61=548#3f@i6m(i_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,20 +1,24 @@
-"""
-Test custom Django management commands
-"""
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
-
+from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch('django.db.utils.ConnectionHandler.__getitem__')
 class CommandTests(SimpleTestCase):
-    def test_wait_for_db_ready(self, patched_check):
-        patched_check.return_value = True
-
+    """Test commands."""
+    def test_wait_for_db_ready(self, patched_getitem):
+        """Test waiting for database if database ready."""
+        patched_getitem.return_value = True
         call_command('wait_for_db')
+        self.assertEqual(patched_getitem.call_count, 1)
 
-        patched_check.assert_called_once_with(database=['default'])
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
+        """Test waiting for database when getting OperationalError."""
+        patched_getitem.side_effect = [Psycopg2OpError] * 2 + \
+                                      [OperationalError] * 3 + [True]
+        call_command('wait_for_db')
+        self.assertEqual(patched_getitem.call_count, 6)
